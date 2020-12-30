10aacfe1129ce83fa3511ee4e84c159aca9f6ec0
Shreyas Shrawage <shreyas.shrawage2000@gmail.com>
2020-12-30 23:04:46 +0530

    added sort button

--- sketch.js
+++ sketch.js
@@ -1,21 +1,35 @@
 let index_i = 0;
 let index_j = 0;
 
-let size = 40;
+let size = 40; // size of the bars (40 or greater displays numbers)
+let apply_sleep = false; // enhances swap animation at low fr
 let fr = 300; // 1 - slowest, 300 - fastest
 
 let array = [];
 let sorted = false;
 
+let startSorting = false;
+
 function createArray(windowWidth) {
   for (i = 0; i < Math.floor(windowWidth / size); i++)
-    array.push(Math.floor(random(20, height - 20)));
+    array.push(Math.floor(random(20, height - 100)));
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   createArray(windowWidth);
   frameRate(fr);
+
+  const sortButton = createButton("Sort!");
+  sortButton.position(windowWidth / 2, windowHeight - 50);
+  sortButton.size(90, 40);
+  sortButton.style(
+    "background-color : transparent; color : white; outline: none; border-color: rgb(200, 0, 200, 100); border-radius: 20px"
+  );
+  sortButton.mousePressed(() => {
+    startSorting = true;
+    sortButton.style("display: none");
+  });
 }
 
 const sleep = () =>
@@ -27,7 +41,6 @@ const sleep = () =>
 
 async function draw() {
   background(50);
-
   for (let i = 0; i < array.length; i++) {
     stroke(200, 100, 100);
     fill(200, 0, 200, 100);
@@ -38,46 +51,48 @@ async function draw() {
     if (size >= 40) text(array[i], i * size + size / 4, array[i] - 10);
   }
 
-  await sleep();
-  // BUBBLE SORT
+  if (startSorting) {
+    if (apply_sleep) await sleep();
+    // BUBBLE SORT
 
-  let swap;
+    let swap;
 
-  if (index_j < array.length - 1) {
-    if (index_i < array.length - index_j - 1) {
-      if (array[index_i] > array[index_i + 1]) {
-        swap = array[index_i];
-        array[index_i] = array[index_i + 1];
-        array[index_i + 1] = swap;
+    if (index_j < array.length - 1) {
+      if (index_i < array.length - index_j - 1) {
+        if (array[index_i] > array[index_i + 1]) {
+          swap = array[index_i];
+          array[index_i] = array[index_i + 1];
+          array[index_i + 1] = swap;
 
-        clear();
-        background(50);
-        for (let i = 0; i < array.length; i++) {
-          stroke(200, 100, 100);
-          fill(200, 0, 200, 100);
-          if (
-            (array[i] && array[i + 1] && i === index_i) ||
-            i === index_i + 1
-          ) {
-            fill(10, 0, 100);
+          clear();
+          background(50);
+          for (let i = 0; i < array.length; i++) {
+            stroke(200, 100, 100);
+            fill(200, 0, 200, 100);
+            if (
+              (array[i] && array[i + 1] && i === index_i) ||
+              i === index_i + 1
+            ) {
+              fill(10, 0, 100);
+            }
+            rect(i * size, 0, size, array[i]);
+            if (size >= 40) text(array[i], i * size + size / 4, array[i] - 10);
           }
-          rect(i * size, 0, size, array[i]);
-          if (size >= 40) text(array[i], i * size + size / 4, array[i] - 10);
         }
+
+        index_i++;
+      } else {
+        index_i = 0;
+        index_j++;
       }
-
-      index_i++;
     } else {
-      index_i = 0;
-      index_j++;
+      sorted = true;
     }
-  } else {
-    sorted = true;
-  }
 
-  if (sorted) {
-    console.log("Sorted!");
-    noLoop();
-    return;
+    if (sorted) {
+      console.log("Sorted!");
+      noLoop();
+      return;
+    }
   }
 }
